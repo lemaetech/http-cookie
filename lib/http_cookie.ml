7bc08ba06e6ad32937fbff5cb9f3b1a041a1876b
Bikal Lem <gbikal@gmail.com>
2021-08-15 15:12:57 +0100

    add ipv4 syntax

--- lib/http_cookie.ml
+++ lib/http_cookie.ml
@@ -272,6 +272,15 @@ let domain_name =
   else return (String.concat "." subdomain) )
   <?> "domain_value"
 
+(*
+     IPv4address = d8 "." d8 "." d8 "." d8
+
+        d8          = DIGIT               ; 0-9
+                    / %x31-39 DIGIT       ; 10-99
+                    / "1" 2DIGIT          ; 100-199
+                    / "2" %x30-34 DIGIT   ; 200-249
+                    / "25" %x30-35        ; 250-255
+*)
 let ipv4_address =
   let d8 =
     lift2
